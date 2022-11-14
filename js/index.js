b93e086b7e32f062ca0e9b92340f2118652deaac
xinyu_H <2431096328@qq.com>
2022-11-14 10:50:32 +0800

    commit

--- js/index.js
+++ js/index.js
@@ -13,9 +13,41 @@ const app = new Vue({
     },
     methods: {
         clickBtn() {
-            
+            // 两个时间都输入了
+            if(this.startDate.trim() && this.endDate.trim()) {
+                if(!this.checkDate(this.startDate.trim()) || !this.checkDate(this.endDate.trim())) {
+                    return vant.Toast('你输入的时间格式不对')
+                }
+                this.getDateTime(this.startDate, this.endDate)
+            } else {
+                if(!this.checkDate(this.startDate.trim() || this.endDate.trim())) {
+                    return vant.Toast('你输入的时间格式不对')
+                }
+                this.getDateTime(this.startDate || this.endDate)
+            }
         },
-
+        getDateTime(start, end = new Date()) {
+            let sD = new Date(start).getTime()
+            let eD = new Date(end).getTime()
+            let diff = (eD - sD) / 1000
+            let d = this.patch(diff / (24 * 3600));
+            let h = this.patch(diff / 3600 % 24);
+            let m = this.patch(diff % 3600 / 60);
+            let s = this.patch(diff % 60);
+            this.timeStr = `${d}天${h}时${m}分${s}秒`
+        },
+        empty() {
+            this.startDate = ''
+            this.endDate = ''
+            this.timeStr = ''
+        },
+        patch(str){
+            str = parseInt(str)
+            return +str <= 0 ? '00' : +str < 10 ? '0' + str : str;
+        },
+        checkDate(str) {
+            return /^\d{4}\/\d{2}\/\d{2}$/i.test(str) || /^\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}$/i.test(str)
+        }
     },
     mounted () {
         
